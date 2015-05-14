7e38c7718e887b8f2dd06320b62dad38463e4cd4
zealer <zehaojin@gmail.com>
2015-05-14 23:51:23 +0800

    add terrain heightmap

--- SnowTest/Terrain.cpp
+++ SnowTest/Terrain.cpp
@@ -1,5 +1,23 @@
 #include "Terrain.h"
+#include <fstream>
+
+Terrain::Terrain():heightmapData(0){
+
+}
 
 Terrain::~Terrain()
 {
+}
+
+void Terrain::initHeightMap(std::string rawHeightmapPath , int size){
+	this->heightmapSize = size;
+	std::ifstream fin(rawHeightmapPath, std::ios::binary);
+
+	heightmapData = new unsigned char[size*size];
+	
+	fin.read((char *)heightmapData, size*size*sizeof(char));
+
+	fin.close();
+
+
 }
--- SnowTest/Terrain.h
+++ SnowTest/Terrain.h
@@ -3,13 +3,17 @@
 
 #include <d3d9.h>
 #include <d3dx9.h>
+#include <string>
 
 class Terrain
 {
 private:
-
+	int heightmapSize;
+	unsigned char *heightmapData;
 
 public:
+	void initTexture(std::string path);
+	void initHeightMap(std::string rawHeightmapPath , int size);//高度图位置与边长
 	void draw();
 
 public:
