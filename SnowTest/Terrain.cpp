7f5bd372222599027b9a687fa1c0264e82a2cb6b
zehao <zehaojin@gmail.com>
2015-06-15 17:56:09 +0800

    add

--- SnowTest/Terrain.cpp
+++ SnowTest/Terrain.cpp
@@ -1,5 +1,6 @@
 #include "Terrain.h"
 #include <fstream>
+#include <assert.h>
 
 const DWORD TerrainVertex::FVF = D3DFVF_XYZ | D3DFVF_TEX1;
 
@@ -32,6 +33,61 @@ void Terrain::initTexture(std::string path){
 	D3DXCreateTextureFromFile(this->dev, path.c_str(), &(this->tex));
 }
 
+int Terrain::operator()(int row, int col) const{
+	return heightmapData[row*heightmapSize + col];
+}
+
+void Terrain::generateVertex(){
+	
+	HRESULT res = dev->CreateVertexBuffer(sizeof(TerrainVertex)*heightmapSize*heightmapSize, D3DUSAGE_WRITEONLY,
+									TerrainVertex::FVF, D3DPOOL_MANAGED, &vbuf, 0);
+	assert(SUCCEEDED(res));
+
+	/*
+	设置地形的行列数为heightmap的size。
+	*/
+	int numRow = heightmapSize - 1;
+	int numCol = heightmapSize - 1;
+	float delta = 1.0f / numRow;
+
+	//设置顶点数据
+
+	TerrainVertex *vertex = NULL;
+
+	vbuf->Lock(0, heightmapSize*heightmapSize, (void **)&vertex, 0);
+
+	int row = 0;
+	for (int x = -heightmapSize / 2; x <= heightmapSize / 2; x += 2){
+		int col = 0;
+		for (int z = heightmapSize / 2; z >= -heightmapSize / 2; z -= 2){
+			int idx = row*heightmapSize + col;
+			vertex[idx] = { x, ((float)heightmapData[idx]) / 255 * TERRAIN_MAX_HEIGHT, z, col*delta, row * delta };
+			col++;
+		}
+		row++;
+	}
+
+	vbuf->Unlock();
+
+
+	//设置顶点索引数据
+	int numTriangles = (heightmapSize - 1)*(heightmapSize - 1) * 2;
+	res = dev->CreateIndexBuffer(numTriangles * sizeof(WORD), D3DUSAGE_WRITEONLY, D3DFMT_INDEX16, D3DPOOL_MANAGED, &ibuf, 0);
+
+	assert(SUCCEEDED(res));
+
+	WORD *index = 0;
+
+	for (int i = 0; i < numRow; i++){
+		for (int j = 0; j < numCol; j++){
+
+		}
+	}
+
+}
+
 void Terrain::draw(){
+
+
 
 }
--- SnowTest/Terrain.h
+++ SnowTest/Terrain.h
@@ -16,8 +16,8 @@
 const int TERRAIN_MAX_HEIGHT = 100;
 
 struct TerrainVertex{
-	int x, y, z;
-	int u, v;
+	float x, y, z;
+	float u, v;
 	static const DWORD FVF;
 };
 
@@ -28,12 +28,16 @@ private:
 	unsigned char *heightmapData;  //地形数据
 	IDirect3DTexture9 *tex;              //纹理
 	IDirect3DDevice9* dev;
-
+	IDirect3DVertexBuffer9* vbuf;  //顶点缓冲
+	IDirect3DIndexBuffer9* ibuf;   //索引缓冲
 
 public:
 	void initTexture(std::string path);
 	void initHeightMap(std::string rawHeightmapPath , int size);//高度图位置与边长
 	void draw();
+	void generateVertex();
+	
+	int operator ()(int row, int col) const; //支持括号访问高度图
 
 public:
 	Terrain(IDirect3DDevice9* dev);
